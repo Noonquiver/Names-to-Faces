2161292582742766bec5d104a8022d04c477e169
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-03 18:57:16 -0500

    Collection view controller and personalized collection cell added.

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Camilo Hernández Guerrero on 3/07/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
